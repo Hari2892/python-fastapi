306b9d4c7bfeff3ea4e8b4cb1593c2cb4c8c9c67
Hariharan <hariharan@ventunotech.com>
2026-04-24 10:32:06 +0530

    fastAPI Learning

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello Hari, FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
